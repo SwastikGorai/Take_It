895e41fadec8efac4f7ac8c89d71a474fb76f24f
Swastik Gorai <swastikgorai@gmail.com>
2023-01-20 07:36:57 +0530

    login page done

--- app/src/main/java/com/sevenzerotwo/takeit/pages/authpage2.kt
+++ app/src/main/java/com/sevenzerotwo/takeit/pages/authpage2.kt
@@ -3,17 +3,24 @@ package com.sevenzerotwo.takeit.pages
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.node.ModifierNodeElement
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -22,64 +29,163 @@ import com.sevenzerotwo.takeit.R
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun Authpage2()
-{
+fun Authpage2() {
     Scaffold(
         Modifier
             .fillMaxSize()
-            .padding(16.dp)) {
+            .padding(2.dp)
+    ) {
 
-        Box(Modifier.background(Color(0xFFE6E6FA)).padding(12.dp))
-        {
-            Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Column(Modifier.fillMaxSize().padding(8.dp)) {
 
-                Image(
-                    painter = painterResource(id = R.drawable.illustration),
-                    contentDescription = "Illustration",
+
+            Box(
+                Modifier
+                    .padding(4.dp)
+                    .clip(shape = RoundedCornerShape(24.dp))
+                    .background(Color(0xFFE6E6FA))
+                    .weight(0.9F)
+
+            )
+            {
+                Column(
+                    Modifier.fillMaxSize().padding(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+
+                    Image(
+                        painter = painterResource(id = R.drawable.illustration),
+                        contentDescription = "Illustration",
 //                    colorFilter = ColorFilter.tint(Color(0xfff1f3f4)),
 //                    modifier = Modifier
 //                        .width(width = 316.dp)
 //                        .height(height = 273.dp)
-                )
-
-                Text(
-                    text = "Explore the app",
-                    color = Color.Black,
-                    lineHeight = 130.sp,
-                    style = TextStyle(
-                        fontSize = 32.sp,
-                        fontWeight = FontWeight.Bold
                     )
-                )
-                Spacer(modifier = Modifier.height(8.dp))
 
-                Text(
-                    text = "Shop and earn cool rewards!",
-                    color = Color.Black.copy(alpha = 0.7f),
-                    textAlign = TextAlign.Center,
-                    lineHeight = 125.sp,
-                    style = TextStyle(
-                        fontSize = 17.sp
-                    ),
-                    modifier = Modifier
-                        .width(width = 319.dp)
-                )
+                    Text(
+                        text = "Explore the app",
+                        color = Color.Black,
+                        lineHeight = 130.sp,
+                        style = TextStyle(
+                            fontSize = 32.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    )
+                    
 
-                Button(
-                    onClick = { },
-                    border = BorderStroke(1.dp, Color.White),
-                    modifier = Modifier.fillMaxWidth().height(60.dp),
-                    colors = ButtonDefaults.buttonColors(containerColor = Color.White),
-                    shape = RoundedCornerShape(50)
+                    Text(
+                        text = "Shop and earn cool rewards!",
+                        color = Color.Black.copy(alpha = 0.7f),
+                        textAlign = TextAlign.Center,
+                        lineHeight = 125.sp,
+                        style = TextStyle(
+                            fontSize = 17.sp
+                        ),
+                        modifier = Modifier
+                            .width(width = 319.dp)
+                    )
+                    
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    Button(
+                        onClick = { },
+                        border = BorderStroke(1.dp, Color.Gray),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                        shape = RoundedCornerShape(50)
+                    )
+                    {
+                        Row(
+                            Modifier.fillMaxSize(),
+                            horizontalArrangement = Arrangement.Start,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.google),
+                                contentDescription = "google",
+                                modifier = Modifier.padding(end = 12.dp)
+                            )
+
+                            Text(
+                                text = "Continue with Google",
+                                fontSize = 16.sp,
+                                color = Color.Black,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+//
+                    }
+
+                    Button(
+                        onClick = { },
+                        border = BorderStroke(1.dp, Color.Gray),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(60.dp),
+                        colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                        shape = RoundedCornerShape(50)
+                    )
+                    {
+                        Row(
+                            Modifier.fillMaxSize(),
+                            horizontalArrangement = Arrangement.Start,
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Image(
+                                painter = painterResource(id = R.drawable.message),
+                                contentDescription = "email",
+                                modifier = Modifier.padding(end = 12.dp)
+                            )
+
+                            Text(
+                                text = "Continue with Email",
+                                fontSize = 16.sp,
+                                color = Color.Black,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+//
+                    }
+
+
+                }
+            }
+
+            Box(
+                Modifier
+                    .weight(0.1F)
+                    .fillMaxSize())
+            {
+                Row(
+                    Modifier.fillMaxSize(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
                 )
                 {
+                    Text(
+                        text = "Already have an account? ", fontSize = 16.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Text(
+                        text = "Sign In", fontSize = 16.sp,
+                        color = Color.Black,
+                        fontWeight = FontWeight.Bold,
+                    textDecoration = TextDecoration.Underline,
+                    modifier = Modifier.clickable {  })
+
+
+
+
+                }
+
+
             }
 
-
-
-
-            }
         }
-
     }
 }
